aef3897957cce97a03adb474470088413a548f6b
Karna <kr.newbee@gmail.com>
2024-03-26 12:17:09 -0700

    Name included

--- StartApp.java
+++ StartApp.java
@@ -1,6 +1,8 @@
 public class StartApp {
 
     public static void main (String [] args) {
-        System.out.println(" Sorting Hello Karna - Good Job");
+
+        System.out.println("Hello Karna - Good Job");
     }
 }
+
