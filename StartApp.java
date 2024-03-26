7d58aabf5c36f1a590da92b324f97068f8bcb458
Karunakar Bommareddy <132242085+KarnaJava@users.noreply.github.com>
2024-03-26 12:06:57 -0700

    Update StartApp.java

--- StartApp.java
+++ StartApp.java
@@ -1,6 +1,6 @@
 public class StartApp {
 
     public static void main (String [] args) {
-        System.out.println("Hello Karna - Good Job");
+        System.out.println(" Sorting Hello Karna - Good Job");
     }
 }
